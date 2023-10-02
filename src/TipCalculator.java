06e18446e99cf12f0d16e95541c98036463fe414
Ivan <ravenivan1@gmail.com>
2023-10-02 19:30:57 -0400

    finished

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, double tipPercentage) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+    }
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+    public double getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost) {
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount() {
+        return totalBill() - totalBillBeforeTip;
+    }
+
+    public double totalBill() {
+        return totalBillBeforeTip * (tipPercentage / 100 + 1);
+    }
+
+    public double perPersonCostBeforeTip() {
+        return totalBillBeforeTip / numPeople;
+    }
+
+    public double perPersonTipAmount() {
+        return tipAmount() / numPeople;
+    }
+
+    public double perPersonTotalCost() {
+        return totalBill() / numPeople;
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class TipCalculatorRunner {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> foodList = new ArrayList<String>();
+
+        System.out.println("Welcome to the tip calculator! ");
+        System.out.print("What is your name? ");
+        String name = sc.nextLine(); // stores name of user
+
+        System.out.print("Hello " + name + "!" + " How many people are in your group today?: ");
+        int people = sc.nextInt(); // stores number of people in the group
+
+        System.out.print("What tip percentage would you like to give? (0-100): ");
+        double tip = sc.nextDouble(); // stores tip
+
+        TipCalculator calc = new TipCalculator(people, tip);
+
+        String food;
+        double foodCost = 0;
+
+        while (foodCost != -1) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            foodCost = sc.nextDouble();
+            sc.nextLine();
+            if (foodCost != -1) {
+                calc.addMeal(foodCost);
+                System.out.print("Enter the item: ");
+                food = sc.nextLine();
+                foodList.add(food);
+            }
+        }
+
+        System.out.println("----------------------------------");
+        System.out.printf("Total bill before tip : $%.2f \n", calc.getTotalBillBeforeTip());
+        System.out.println("Total percentage: " + calc.getTipPercentage() + "%");
+        System.out.printf("Total tip: $%.2f ", calc.tipAmount());
+        System.out.printf("\nTotal bill with tip: $%.2f ", calc.totalBill());
+        System.out.printf("\nPer person cost before tip: $%.2f ", calc.perPersonCostBeforeTip());
+        System.out.printf("\nTip per person: $%.2f ", calc.perPersonTipAmount());
+        System.out.printf("\nTotal cost per person: $%.2f ", calc.perPersonTotalCost());
+        System.out.println("\n----------------------------------");
+
+        System.out.println("Items ordered: ");
+        for (String j : foodList) {
+            System.out.println(j);
+        }
+        System.out.println("----------------------------------");
+    }
 }
